5423046bcefbf24da5567ed409cdcbf5d9af5866
Michael <qonfusor@gmail.com>
2025-01-19 19:11:19 -0600

    ngaes: boilerplate done

--- neogeo/aes.cpp
+++ neogeo/aes.cpp
@@ -2,16 +2,71 @@
 #include <cstdlib>
 #include <cstdio>
 #include "aes.h"
+extern console* sys;
 
+void AES::write(u32 addr, u32 v, int size)
+{
+}
 
-
+u32 AES::read(u32 addr, int size)
+{
+	return 0;
+}
 
 bool AES::loadROM(const std::string fname)
 {
-
-
-
-
+	std::string prefix = fname.substr(0, fname.rfind('.'));
+	std::string promfile = prefix + ".p1";
+	FILE* fp = fopen(promfile.c_str(), "rb");
+	if( !fp )
+	{
+		printf("Unable to open <%s>\n", promfile.c_str());
+		return false;
+	}
+	
+	fseek(fp, 0, SEEK_END);
+	auto fsz = ftell(fp);
+	if( fsz < 0x100000 ) fsz = 0x100000;
+	if( fsz & 1 ) fsz += 1;
+	fseek(fp, 0, SEEK_SET);
+	p1.resize(fsz);
+	[[maybe_unused]] int unu = fread(p1.data(), 1, fsz, fp);
+	fclose(fp);
+	
+	for(u32 i = 0; i < p1.size(); i+=2)
+	{
+		*(u16*)&p1[i] = __builtin_bswap16(*(u16*)&p1[i]);
+	}
+	
+	std::string zfile = prefix + ".m1";
+	fp = fopen(zfile.c_str(), "rb");
+	if( !fp )
+	{
+		printf("Unable to open <%s>\n", zfile.c_str());
+		return false;
+	}
+	fseek(fp, 0, SEEK_END);
+	fsz = ftell(fp);
+	if( fsz < 0x10000 ) fsz = 0x10000;
+	fseek(fp, 0, SEEK_SET);
+	m1.resize(fsz);
+	unu = fread(m1.data(), 1, fsz, fp);
+	fclose(fp);
+	
+	std::string fixfile = prefix + ".s1";
+	fp = fopen(fixfile.c_str(), "rb");
+	if( !fp )
+	{
+		printf("Unable to open <%s>\n", fixfile.c_str());
+		return false;
+	}
+	fseek(fp, 0, SEEK_END);
+	fsz = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	s1.resize(fsz);
+	unu = fread(s1.data(), 1, fsz, fp);
+	fclose(fp);
+	return true;
 }
 
 void AES::run_frame()
@@ -23,6 +78,14 @@ void AES::run_frame()
 
 void AES::reset()
 {
-
+	memset(&cpu, 0, sizeof(cpu));
+	cpu.mem_read8 = [](u32 addr)->u8 {return dynamic_cast<AES*>(sys)->read(addr, 8); };
+	cpu.mem_read16 =[](u32 addr)->u16{return dynamic_cast<AES*>(sys)->read(addr, 16); };
+	cpu.mem_read32 =[](u32 addr)->u32{return(dynamic_cast<AES*>(sys)->read(addr, 16)<<16)|(dynamic_cast<AES*>(sys)->read(addr+2, 16)); };
+	cpu.mem_write8 =[](u32 addr, u8 v){ dynamic_cast<AES*>(sys)->write(addr, v, 8); };
+	cpu.mem_write16 =[](u32 addr, u16 v){dynamic_cast<AES*>(sys)->write(addr, v, 16); };
+	cpu.mem_write32 =[](u32 addr, u32 v){dynamic_cast<AES*>(sys)->write(addr, v>>16, 16); dynamic_cast<AES*>(sys)->write(addr+2, v, 16); };
+		
+	spu.reset();
 }
 
--- neogeo/aes.h
+++ neogeo/aes.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <vector>
 #include "console.h"
+#include "68k.h"
+#include "z80.h"
 
 class AES : public console
 {
@@ -7,11 +10,24 @@ public:
 	void run_frame() override;
 	void reset() override;
 	bool loadROM(const std::string) override;
+
+	u32 read(u32 addr, int size);
+	void write(u32 addr, u32 v, int size);
+
+
+	z80 spu;
+	m68k cpu;
 	
 	u32 fb_width() override { return 320; }
-	u32 fb_height() override { return 240; }
+	u32 fb_height() override { return 224; }
 	u8* framebuffer() override { return (u8*)&fbuf[0]; }
-
+	
+	std::vector<u8> p1;
+	std::vector<u8> s1;
+	std::vector<u8> m1;
+	
+	u16 vram[34*1024];
+	u64 wram[0x10000];
 	u32 fbuf[320*240];
 };
 
